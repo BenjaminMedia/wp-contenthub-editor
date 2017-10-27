ba75f1b9307d5a2b7a06b936d4dd2b40e9137d7c
Alf Henderson <alfhenderson@gmail.com>
2017-10-27 15:33:14 +0200

    BUGFIX: wrong permalink

--- contenthub-editor.php
+++ contenthub-editor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: ContentHub Editor
- * Version: 1.1.19
+ * Version: 1.1.20
  * Plugin URI: https://github.com/BenjaminMedia/contenthub-editor
  * Description: This plugin integrates Bonnier Contenthub and adds a custom post type Composite
  * Author: Bonnier - Alf Henderson
