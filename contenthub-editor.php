60020c8eb6d06742cfcfd85f7e5c622cff5b3ad3
Alf Henderson <alfhenderson@gmail.com>
2017-11-10 14:24:10 +0100

    Updated plugin version

--- contenthub-editor.php
+++ contenthub-editor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: ContentHub Editor
- * Version: 1.1.24
+ * Version: 1.1.25
  * Plugin URI: https://github.com/BenjaminMedia/contenthub-editor
  * Description: This plugin integrates Bonnier Contenthub and adds a custom post type Composite
  * Author: Bonnier - Alf Henderson
