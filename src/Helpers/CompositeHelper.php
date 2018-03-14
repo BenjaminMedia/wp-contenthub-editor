37f0bc8bf5c0fa001a46a0a4a523752c1de1ceca
Mohamed Salem Lamiri <lms-pgm@hotmail.fr>
2018-03-14 15:44:56 +0100

    change while loop to a collection

--- src/Helpers/CompositeHelper.php
+++ src/Helpers/CompositeHelper.php
@@ -21,20 +21,16 @@ class CompositeHelper
             return;
         }
 
-        while (have_rows('composite_content', $postId)) {
-            //this function will increment have_rows after every loop
-            $widget = the_row();
+        $videoWithTeaser =collect(get_field('composite_content'))->first(function($content){
+            return $content['teaser_image'] && $content['acf_fc_layout'] === 'video' ;
+        });
 
-            //field_5a8d7ae021e44 is the hardcoded ACF name for video teaser_image
-            if (!$widget['field_5a8d7ae021e44'] || $widget['acf_fc_layout'] !== 'video') {
-                continue;
-            }
-
-            $embed = get_sub_field('embed_url');
+        if($videoWithTeaser){
+            $embed = $videoWithTeaser['embed_url'];
             $teaserImagefile = VideoHelper::getLeadImageFile($embed, 'https://bonnier-publications-danmark.23video.com');
 
             if (!$teaserImagefile->url || empty($embed)) {
-                continue;
+                return;
             }
 
             try {
@@ -45,12 +41,10 @@ class CompositeHelper
                 if ($attachmentId && ((empty($videoTeaserImageUrl)) || $videoTeaserImageUrl[0] !== $teaserImagefile->url)) {
                     update_field('teaser_image', $attachmentId);
                     update_post_meta($postId, 'video_teaser_image_origin_url', $teaserImagefile->url);
-                    break;
                 }
             } catch (\Exception $e) {
-                echo 'Error uploading video thumbnail! '.$e->getMessage();
+                echo 'Error uploading video thumbnail! ' . $e->getMessage();
             }
         }
     }
-
 }
