06a2012e298f6deb84c1f2981b4c297d75d9da29
Magnus Flor Johansen <magnus@m-flor.dk>
2018-06-15 09:40:58 +0200

    WILL-158 - Easy tables - Added new JS for the Markdown editor - Updated Text item to support new markdown editor configs - Updated Gallery item to support inline gallery display hint.

--- js/acf/fields/markdown-editor.js
+++ js/acf/fields/markdown-editor.js
@@ -1,31 +1,74 @@
 (function () {
-    function createSimpleMde(textArea, options) {
-        var mdeOptions = {
-            element: textArea,
-            spellChecker: true,
-        };
-        if(typeof dictionary !== "undefined") {
-            mdeOptions.dictionary = dictionary;
-        }
-        new SimpleMDE(Object.assign(mdeOptions, options));
+  function quote(editor) {
+    var cm = editor.codemirror;
+    var selectedText = cm.getSelection();
+    var text = selectedText || '';
+    cm.replaceSelection('<cite>' + text + '</cite>');
+  }
+
+  function createSimpleMde(textArea, options) {
+    var mdeOptions = {
+      element: textArea,
+      spellChecker: true,
     };
-    
-    acf.add_action('append', function( el )
-    {
-        var textArea = jQuery(el).find('.acf-field-simple-mde');
-        if(typeof textArea[0] !== 'undefined')
-            createSimpleMde(textArea[0], textArea.data('simple-mde-config'));
-    });
-    
-    acf.add_action('ready', function( el ){
-        
-        jQuery(el).find('.acf-field-simple-mde').each(function()
+    if (typeof dictionary !== "undefined") {
+      mdeOptions.dictionary = dictionary;
+    }
+    var toolbar = {
+      toolbar: [
+        "bold",
+        "italic",
+        "quote",
         {
-            if(jQuery(this).is(":visible")) { // Only render visible elements
-                createSimpleMde(this, jQuery(this).data('simple-mde-config'));
-            }
-        })
-        
-    });
-    
+          name: "cite",
+          action: quote,
+          className: "fa fa-star",
+          title: "Citation",
+        },
+        "heading-2",
+        "heading-3",
+        "|",
+        "unordered-list",
+        "ordered-list",
+        "link",
+        "|",
+        "preview",
+        "guide"
+      ]
+    };
+
+    if ('simple' === options) {
+      toolbar = {
+        toolbar: [
+          "bold",
+          "italic",
+          "|",
+          "unordered-list",
+          "ordered-list",
+          "link",
+          "|",
+          "preview",
+          "guide"
+        ]
+      };
+    }
+    new SimpleMDE(Object.assign(mdeOptions, toolbar));
+  };
+
+  acf.add_action('append', function (el) {
+    var textArea = jQuery(el).find('.acf-field-simple-mde');
+    if (typeof textArea[0] !== 'undefined' && textArea.is(':visible'))
+      createSimpleMde(textArea[0], textArea.data('simple-mde-config'));
+  });
+
+  acf.add_action('ready', function (el) {
+
+    jQuery(el).find('.acf-field-simple-mde').each(function () {
+      if (jQuery(this).is(":visible")) { // Only render visible elements
+        createSimpleMde(this, jQuery(this).data('simple-mde-config'));
+      }
+    })
+
+  });
+
 })();
