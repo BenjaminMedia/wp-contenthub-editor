cc6e2e276c4d03c54854d35f72bb4715ad96d274
Magnus Flor Johansen <magnus@m-flor.dk>
2018-08-13 15:12:54 +0200

    WILL-840 - Willow backend release

--- contenthub-editor.php
+++ contenthub-editor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: ContentHub Editor
- * Version: 3.0.0
+ * Version: 3.0.1
  * Plugin URI: https://github.com/BenjaminMedia/contenthub-editor
  * Description: This plugin integrates Bonnier Contenthub and adds a custom post type Composite
  * Author: Bonnier - Alf Henderson
