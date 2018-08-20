aaec3ae10fa51bf2a1c6898dcd92083f18179ccd
Frederik Rabøl <frederik-rm@hotmail.com>
2018-08-20 15:32:52 +0200

    changed version number

--- contenthub-editor.php
+++ contenthub-editor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: ContentHub Editor
- * Version: 2.0.2
+ * Version: 2.0.3
  * Plugin URI: https://github.com/BenjaminMedia/contenthub-editor
  * Description: This plugin integrates Bonnier Contenthub and adds a custom post type Composite
  * Author: Bonnier - Alf Henderson
