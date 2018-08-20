daf005de9063eaf48066579bf3f0146936156c69
Xayer <frederik-rm@hotmail.com>
2018-08-20 13:00:58 +0000

    Apply fixes from StyleCI

--- src/Settings/SettingsPage.php
+++ src/Settings/SettingsPage.php
@@ -66,7 +66,7 @@ class SettingsPage
      */
     public function create_admin_page()
     {
-        // Set class property ?>
+        // Set class property?>
         <div class="wrap">
             <form method="post" action="options.php">
                 <?php
