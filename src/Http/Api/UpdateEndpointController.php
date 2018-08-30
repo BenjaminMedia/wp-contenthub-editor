eb8449c6e26bf8280e7c55266365336e62896a36
Alf Henderson <alfhenderson@gmail.com>
2018-08-30 15:15:15 +0200

    WILL-859/BUGFIX: Categories that shared slugs in multiple languages

--- src/Http/Api/UpdateEndpointController.php
+++ src/Http/Api/UpdateEndpointController.php
@@ -3,6 +3,7 @@
 namespace Bonnier\WP\ContentHub\Editor\Http\Api;
 
 use Bonnier\WP\ContentHub\Editor\Helpers\TermImportHelper;
+use PLL_Admin_Share_Term_Slug;
 use WP_REST_Controller;
 use WP_REST_Request;
 use WP_REST_Response;
@@ -22,6 +23,8 @@ class UpdateEndpointController extends WP_REST_Controller
 
     public function updateCallback(WP_REST_Request $request): WP_REST_Response
     {
+        $this->initPolylangShareTermSlug(); // Make sure that category slugs are sharable across languages
+
         $resource = $this->formatResource($request->get_param('data'));
         $meta = $request->get_param('meta');
         $entityType = $meta['entity_type'];
@@ -35,6 +38,7 @@ class UpdateEndpointController extends WP_REST_Controller
             if ($actionType === 'delete') {
                 $termImporter->deleteTermAndTranslations($resource);
             }
+
             return new WP_REST_Response(['status' => 'OK']);
         }
 
@@ -50,5 +54,11 @@ class UpdateEndpointController extends WP_REST_Controller
     {
         // Convert array to object
         return json_decode(json_encode($resource));
+    }
+
+    private function initPolylangShareTermSlug()
+    {
+        $polylang = PLL(); // We have to store in variable because class is using & which is not allowed for objects
+        new PLL_Admin_Share_Term_Slug($polylang);
     }
 }
