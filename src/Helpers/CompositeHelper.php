3b39d3e2a298c05180f00740feaafc67956aab79
Frederik Rabøl <frederik-rm@hotmail.com>
2018-09-04 15:58:54 +0200

    added post_parent on associated_content upon saving story

--- src/Helpers/CompositeHelper.php
+++ src/Helpers/CompositeHelper.php
@@ -13,6 +13,7 @@ class CompositeHelper
     public function __construct()
     {
         add_action('save_post', [$this, 'videoTeaserImage']);
+        add_action('save_post', [$this, 'SetStoryParent']);
     }
 
     /**
@@ -55,5 +56,27 @@ class CompositeHelper
                 update_post_meta($postId, 'video_teaser_image_origin_url', $teaserImagefile->url);
             }
         }
+    }
+
+    public function SetStoryParent($postId){
+        if (wp_is_post_revision($postId) || !have_rows('composite_content', $postId)) {
+            return;
+        }
+
+        //do all this magic code because we don\'t want the wp_update_post to run infinitely.
+
+        if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE )
+            return;
+
+        if ( !current_user_can('edit_post', $postId) )
+            return;
+
+        global $wpdb;
+
+        collect(get_field('composite_content'))->each(function ($content) use($postId, $wpdb) {
+            if($content['acf_fc_layout'] === 'associated_composite'){
+                $wpdb->update('wp_posts', ['post_parent' => $postId], ['ID' => $content['composite'][0]->ID]);
+            }
+        });
     }
 }
