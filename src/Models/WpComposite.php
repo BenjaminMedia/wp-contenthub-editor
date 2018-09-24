821ffec46c4f43d7bd353f8e04d683ce44c85a3c
SorenThorup <soren.thorup@bonnier.dk>
2018-09-24 11:24:11 +0200

    Revert "Removed caption markdown field."
    This reverts commit cd820ff61255be3435b63634296f3d4905ae9b53.

--- src/Models/WpComposite.php
+++ src/Models/WpComposite.php
@@ -90,6 +90,7 @@ class WpComposite
 
     private static function register_acf_fields()
     {
+        AttachmentGroup::register();
         CompositeFieldGroup::register();
         MagazineFieldGroup::register();
         MetaFieldGroup::register();
