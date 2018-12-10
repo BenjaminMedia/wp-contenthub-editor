0267b4d6996f6f8d43d96f47c30ab3019c57d199
Magnus Flor Johansen <magnus@m-flor.dk>
2018-12-10 12:01:00 +0100

    Removing rogue console.log

--- js/acf/fields/page-validation.js
+++ js/acf/fields/page-validation.js
@@ -2,10 +2,7 @@ acf.addFilter('validation_complete', function (json, $form) {
   $ = jQuery;
   var teaserListPaginationsChecked = $(".acf-field-5bb318f2ffcef .acf-flexible-content .values .acf-fields [data-name='pagination'] input:checkbox:checked");
 
-  console.log(teaserListPaginationsChecked.length);
-
   if (teaserListPaginationsChecked.length > 1) {
-    console.log('More Than One!');
     teaserListPaginationsChecked.slice(1).each(function () {
       var paginationError = {input: $(this).attr('name'), message: 'Please make sure you only select 1 paginated teaser list!'};
 
@@ -16,7 +13,6 @@ acf.addFilter('validation_complete', function (json, $form) {
       json.errors.push(paginationError);
 
       json.valid = 0;
-      console.log(json);
     });
   }
 
