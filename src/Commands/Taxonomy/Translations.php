d994833750d0d702f73d8b327d3884529f267450
Jonas Kjaergaard <jonask@jonask.dk>
2019-11-05 15:04:58 +0100

    WILL-904: Style-CI changes

--- src/Commands/Taxonomy/Translations.php
+++ src/Commands/Taxonomy/Translations.php
@@ -37,44 +37,44 @@ class Translations extends BaseTaxonomyImporter
             collect(get_terms(['taxonomy' => $taxonomy->machine_name, 'lang' => 'da', 'hide_empty' => false]))
                 ->each(function ($term) use ($taxonomy) {
 
-                $translatedTerms = pll_get_term_translations($term->term_id);
+                    $translatedTerms = pll_get_term_translations($term->term_id);
 
-                // Get composites with the current term
-                collect(get_posts(
-                    [
-                        'post_type' => 'contenthub_composite',
-                        'numberposts' => -1, // Get all posts
-                        'tax_query' => [
-                            [
-                                'taxonomy' => $taxonomy->machine_name,
-                                'field' => 'term_id',
-                                'terms' => $term->term_id
+                    // Get composites with the current term
+                    collect(get_posts(
+                        [
+                            'post_type' => 'contenthub_composite',
+                            'numberposts' => -1, // Get all posts
+                            'tax_query' => [
+                                [
+                                    'taxonomy' => $taxonomy->machine_name,
+                                    'field' => 'term_id',
+                                    'terms' => $term->term_id
+                                ]
                             ]
                         ]
-                    ]
-                ))->each(function ($post) use ($taxonomy, $translatedTerms) {
+                    ))->each(function ($post) use ($taxonomy, $translatedTerms) {
 
-                    // Get translated composites
-                    collect(pll_get_post_translations($post->ID))->forget('da')
-                        ->each(function ($post_id, $lang) use ($taxonomy, $translatedTerms) {
+                        // Get translated composites
+                        collect(pll_get_post_translations($post->ID))->forget('da')
+                            ->each(function ($post_id, $lang) use ($taxonomy, $translatedTerms) {
 
-                            $term = get_term($translatedTerms[$lang]);
-
-                            if (self::isUpdateNeeded($term, $taxonomy, $post_id)) {
-                                WP_CLI::line('Adding term - ' . $lang);
-                                WP_CLI::line('term: ' . $term->term_id . ' - ' . $term->name);
-                                WP_CLI::line('post: ' . $post_id . ' - ' . get_the_title($post_id));
-                                WP_CLI::line('');
-
-                                wp_set_post_terms($post_id, $term->name, $taxonomy->machine_name, true);
-                                update_field($taxonomy->machine_name, $term->term_id, $post_id);
+                                $term = get_term($translatedTerms[$lang]);
 
                                 if (self::isUpdateNeeded($term, $taxonomy, $post_id)) {
-                                    WP_CLI::error('ERROR adding the term to the post');
+                                    WP_CLI::line('Adding term - ' . $lang);
+                                    WP_CLI::line('term: ' . $term->term_id . ' - ' . $term->name);
+                                    WP_CLI::line('post: ' . $post_id . ' - ' . get_the_title($post_id));
+                                    WP_CLI::line('');
+
+                                    wp_set_post_terms($post_id, $term->name, $taxonomy->machine_name, true);
+                                    update_field($taxonomy->machine_name, $term->term_id, $post_id);
+
+                                    if (self::isUpdateNeeded($term, $taxonomy, $post_id)) {
+                                        WP_CLI::error('ERROR adding the term to the post');
+                                    }
                                 }
-                            }
-                        });
-                });
+                            });
+                    });
             });
         });
     }
