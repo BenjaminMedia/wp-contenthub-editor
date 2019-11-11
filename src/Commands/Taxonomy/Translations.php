e60c72815e12b056fcd8b699192bd3d8cbc0f839
Jonas Kjaergaard <jonask@jonask.dk>
2019-11-11 17:00:25 +0100

    WILL-904: Now gives a warning when a translation is missing (instead of exiting with an error)

--- src/Commands/Taxonomy/Translations.php
+++ src/Commands/Taxonomy/Translations.php
@@ -48,31 +48,26 @@ class Translations extends BaseTaxonomyImporter
                 'lang' => $sourceLang,
             ]
         ))->each(function ($sourcePost) use ($sourceLang, $taxonomiesToSync) {
-
             // Get translated articles (pass on source_article_id)
             collect(pll_get_post_translations($sourcePost->ID))->forget($sourceLang)->each(function ($translatedPostId, $translatedLang) use ($sourcePost, $taxonomiesToSync) {
-                WP_CLI::line('sourcePost->ID: ' . $sourcePost->ID);
-                WP_CLI::line('translatedPost->ID: ' . $translatedPostId);
-
                 // Sync tags and custom taxonomies
                 $taxonomiesToSync->each(function ($taxonomyACF, $taxonomyWP) use ($sourcePost, $translatedPostId, $translatedLang) {
                     self::syncTermsFromPostToPost($taxonomyWP, $taxonomyACF, $sourcePost->ID, $translatedPostId, $translatedLang);
                 });
-
-                WP_CLI::line('');
             });
         });
     }
 
     private function syncTermsFromPostToPost($taxonomyWP, $taxonomyACF, $sourcePostId, $translatedPostId, $translatedLang)
     {
-        $sourcePostHasTagIdsTranslated = collect(get_the_terms($sourcePostId, $taxonomyWP))->map(function ($sourceTag) use ($translatedPostId, $translatedLang) {
+        $sourcePostHasTagIdsTranslated = collect(get_the_terms($sourcePostId, $taxonomyWP))->map(function ($sourceTag) use ($translatedPostId, $translatedLang, $sourcePostId) {
             if (!$sourceTag) {
                 return null;
             }
             $translatedTerms = pll_get_term_translations($sourceTag->term_id);
             if (!isset($translatedTerms[$translatedLang])) {
-                WP_CLI::error('No term-translation found');
+                WP_CLI::warning('No term-translation found (sourcePostId,translatedPostId,term,lang): ' . $sourcePostId . ',' . $translatedPostId . ',' . $sourceTag->name . ',' . $translatedLang);
+                return null;
             }
             return $translatedTerms[$translatedLang];
         });
