706d8a068a3eb18de385b848e3f4236cb58bf754
Jonas Kjaergaard <jonask@jonask.dk>
2019-11-11 17:01:13 +0100

    WILL-904: Incremented version

--- contenthub-editor.php
+++ contenthub-editor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: ContentHub Editor
- * Version: 1.15.1
+ * Version: 1.15.2
  * Plugin URI: https://github.com/BenjaminMedia/contenthub-editor
  * Description: This plugin integrates Bonnier Contenthub and adds a custom post type Composite
  * Author: Bonnier - Alf Henderson
