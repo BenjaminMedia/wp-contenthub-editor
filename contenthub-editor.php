19726c5dd25798c2dafe894e240bdddc5759fb96
SorenThorup <Sorenthorup1@gmail.com>
2020-02-12 12:36:50 +0100

    Version bump.

--- contenthub-editor.php
+++ contenthub-editor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: ContentHub Editor
- * Version: 1.18.0
+ * Version: 1.18.1
  * Plugin URI: https://github.com/BenjaminMedia/contenthub-editor
  * Description: This plugin integrates Bonnier Contenthub and adds a custom post type Composite
  * Author: Bonnier - Alf Henderson
