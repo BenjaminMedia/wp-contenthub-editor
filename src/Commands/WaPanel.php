1a2433a30a255dbb51b29d335abd954e26421e5f
Alf Henderson <alfhenderson@gmail.com>
2020-03-30 13:16:12 +0200

    Bugfix wa panel sort by tags

--- src/Commands/WaPanel.php
+++ src/Commands/WaPanel.php
@@ -121,7 +121,7 @@ class WaPanel extends BaseCmd
                     'type' => collect([ // Map the type
                         'Widgets::Text' => 'seo_text',
                         'Widgets::Standard' => 'teaser_list',
-                        'Widgets::NewsletterSignup' => 'newsletter',
+                        'Widgets::NewsletterSignup' => 'cnewsletter',
                         'Widgets::FullWidthContent' => 'teaser_list',
                         'Widgets::RotatorWithThumbnails' => 'teaser_list',
                     ])->get($waWidget->type, $waWidget->type),
@@ -164,6 +164,22 @@ class WaPanel extends BaseCmd
                         $sortBySkipAmount[$widget->category_id] = $widget->number_of_contents;
                     } else {
                         $sortBySkipAmount[$widget->category_id] += $widget->number_of_contents;
+                    }
+                    return $teaserListWidget;
+                }
+                if ($widget->type === 'teaser_list' && $widget->tags) {
+                    $teaserListWidget = [
+                        'acf_fc_layout' => $widget->type,
+                        'display_format' => $widget->display_format,
+                        'tag' => WpTerm::id_from_whitealbum_id($widget->tags),
+                        'sort_by' => 'custom',
+                        AcfName::FIELD_TEASER_AMOUNT => $widget->number_of_contents,
+                        AcfName::FIELD_SKIP_TEASERS_AMOUNT => $sortBySkipAmount[$widget->tags] ?? 0,
+                    ];
+                    if (!isset($sortBySkipAmount[$widget->tags])) {
+                        $sortBySkipAmount[$widget->tags] = $widget->number_of_contents;
+                    } else {
+                        $sortBySkipAmount[$widget->tags] += $widget->number_of_contents;
                     }
                     return $teaserListWidget;
                 }
