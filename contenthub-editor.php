5e6727d5983b9dfac847580b3602c65301062d25
Mads Paaskesen <madspaaskesen@gmail.com>
2022-09-09 17:44:18 +0200

    upgrade-acf-to-5.12.2
    Opgrade to php74 with composer packages updates etc...

--- contenthub-editor.php
+++ contenthub-editor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: ContentHub Editor
- * Version: 5.28.2
+ * Version: 5.28.3
  * Plugin URI: https://github.com/BenjaminMedia/contenthub-editor
  * Description: This plugin integrates Bonnier Contenthub and adds a custom post type Composite
  * Author: Bonnier - Alf Henderson
